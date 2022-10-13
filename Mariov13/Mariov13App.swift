af58cfc66bf35bd98de8af159f362e28033f86cc
Mario <marioehiagwina03@icloud.com>
2022-10-13 09:17:33 +0200

    Initial Commit

---
+++ Mariov13/Mariov13App.swift
@@ -0,0 +1,17 @@
+//
+//  Mariov13App.swift
+//  Mariov13
+//
+//  Created by Mario Ehiagwina on 2022-10-13.
+//
+
+import SwiftUI
+
+@main
+struct Mariov13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
